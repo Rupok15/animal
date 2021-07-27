56e9f2479e5b5551b03ab2beadb7f2a353fb24ad
Rupok15 <76992981+Rupok15@users.noreply.github.com>
2021-07-27 12:35:34 +0600

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,41 @@
+(function($) {
+    'use strict';
+
+    $('.html').rProgressbar({
+        percentage: 40,
+        fillBackgroundColor: '#1abc9c'
+    });
+    $('.css').rProgressbar({
+        percentage: 50,
+        fillBackgroundColor: '#2ecc71'
+    });
+
+
+
+    $('.php').rProgressbar({
+        percentage: 60,
+        fillBackgroundColor: '#9b59b6'
+    });
+
+
+
+    $('.javascript').rProgressbar({
+        percentage: 70,
+        fillBackgroundColor: '#34495e'
+    });
+
+
+
+    $('.jquery').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#f1c40f',
+        backgroundColor: '#e67e22'
+    });
+
+
+
+
+
+
+
+})(jQuery);
